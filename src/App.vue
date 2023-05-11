38880d049ed788fa8736658a93cef9f8ca8126e6
I Gusti Ngurah Cipta Dwipajaya <gusti.dwipajaya@digits.id>
2023-05-11 20:48:13 +0700

    Final Push

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
 			<h1 class="text-gray-700 p-4 bg-white shadow-md rounded-md font-bold text-xl uppercase">Simple Todo App</h1>
 		</div>
 		<div class="bg-white mx-8 p-4 shadow-md rounded-md text-lg">
-			<div class="grid grid-cols-3 gap-2">
+			<div class="grid grid-cols-3 gap-2 mb-8">
 				<input 
 					@keyup.enter="add" v-model="todo" type="text" 
 					class="bg-slate-100 col-span-2 border border-blue-500 p-2 rounded-md focus:outline focus:outline-cyan-500" placeholder="Masukan todo"
 				>
 				<button @click="add" class="bg-gradient-to-r from-cyan-500 to-blue-500 px-2 py-1 rounded-md text-white shadow-md">Add📆</button>
 			</div>
-			<List :todos="todos"/> <!--mengirim data binding dari parent ke child component-->
+			<!--mengirim data binding dari parent ke child component dan @deleteTodoList adalah emit yang dikirim dari child component-->
+			<List :todos="todos" @deleteTodoList="deleteTodo" @doneTodoList="doneTodo" class="mb-6"/> 
+			<small>Total Todo: {{ totalTodos }}</small>
 		</div>
 	</div>
 </template>
@@ -26,11 +28,52 @@
 				todos: []
 			}
 		},
+
+		computed: {
+			totalTodos(){
+				return this.todos.length
+			}
+		},
+
 		methods: {
 			add(){
-				this.todos.push(this.todo);
+				//menambahkan / menampilkan data dari depan
+				this.todos.unshift({
+					activity: this.todo,
+					isDone: false
+				}); 
 				this.todo = "" //mengosongkan form input setelah enter atau add
-			}
-		}
-	}
+				this.saveToLocalStorage();
+			},
+
+			//jika index dari array todos tidak sama dengan todoIndex yang dikirim dari emit childcomponent, maka kembalikan dan tampilkan item dari setiap array todos. 
+			//Jika nilai index array todos sama dengan todoIndex maka item tersebut jangan tampilkan(dihapus)
+			deleteTodo(todoIndex){
+				this.todos = this.todos.filter((item, index) => {
+					if(index != todoIndex){ 
+						return item
+					}
+				});
+				this.saveToLocalStorage();
+			},
+
+			doneTodo(todoIndex){
+				this.todos = this.todos.filter((item, index)=> {
+					if(index === todoIndex){
+						item.isDone = !item.isDone 
+					}
+					return item;
+				});
+				this.saveToLocalStorage();
+			},
+
+			saveToLocalStorage(){
+				localStorage.setItem('todos', JSON.stringify(this.todos)); //menyimpan ke localstorage dan mengubah ke dlm bentuk string
+			},			
+		},
+
+		mounted() {
+			this.todos = JSON.parse(localStorage.getItem('todos')); //get todos dari localstorage dan mengubahnya ke dlm bentuk json
+		},
+	};
 </script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,15 +1,37 @@
 <template>
 	<ul>
-		<li v-for="(todo, index) in todos" :key="index">{{ index+1 }}. {{ todo }}</li>
+		<li v-for="(todo, index) in todos" :key="index" class="my-2 flex justify-between">
+			<div v-if="todo.isDone">
+				<del>{{ index+1 }}. {{ todo.activity }}</del>
+			</div>
+			<div v-else>
+				<span>{{ index+1 }}. {{ todo.activity }}</span>
+			</div>
+			<div class="flex gap-4">
+				<button @click="doneTodo(index)">✅</button>
+				<button @click="deleteTodo(index)">❌</button>
+			</div>
+		</li>
 	</ul>
 </template>
 
 <script>
 	export default{
+		//menyiapkan props untuk menerima data todos dari parent
 		props: {
 			todos: {
 				type: Array,
 				default: []
+			}
+		},
+		methods: {
+			deleteTodo(index){
+				this.$emit('deleteTodoList', index);
+			},
+			
+			doneTodo(index){
+				this.$emit('doneTodoList', index); 
+				//parameter pertama nama emit untuk dipanggil di parent dan parameter kedua adalah value yang dikirim
 			}
 		}
 	}
